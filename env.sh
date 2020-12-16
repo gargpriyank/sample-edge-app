ba32fbe4c453c48facd064a550f1d52d74ccfc7b
Priyank Garg <42581653+gargpriyank@users.noreply.github.com>
2020-12-15 19:37:08 -0600

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,7 +1,7 @@
-docker login -u sandboxedgeworkshop -p VWCugI4eTOi69vMYDKXXgLxB3rQcXEY2
+docker login -u sandboxedgeworkshop -p <password>
 
-export HZN_ORG_ID=sandbox-edge-workshop-ieam-42-cluster
-export HZN_EXCHANGE_USER_AUTH=iamapikey:8ExP6GCzKoQkRXT_5xktonHIsG0fjKYZItLwqZqym__6
-export EVENTSTREAMS_TOPIC_NAME=sandbox-workshop-edge-topic-1
-export EVENTSTREAMS_BROKER_URL="broker-0-f3wdhjkm0cc4w4tp.kafka.svc06.us-south.eventstreams.cloud.ibm.com:9093,broker-1-f3wdhjkm0cc4w4tp.kafka.svc06.us-south.eventstreams.cloud.ibm.com:9093,broker-5-f3wdhjkm0cc4w4tp.kafka.svc06.us-south.eventstreams.cloud.ibm.com:9093,broker-2-f3wdhjkm0cc4w4tp.kafka.svc06.us-south.eventstreams.cloud.ibm.com:9093,broker-4-f3wdhjkm0cc4w4tp.kafka.svc06.us-south.eventstreams.cloud.ibm.com:9093,broker-3-f3wdhjkm0cc4w4tp.kafka.svc06.us-south.eventstreams.cloud.ibm.com:9093"
-export EVENTSTREAMS_API_KEY=zQPN8nXJIkPKLeEfHcawNVhGCXkyG-91Y6OzV7I43_Qm
+export HZN_ORG_ID=<ieam_organization_id>
+export HZN_EXCHANGE_USER_AUTH=iamapikey:<ieam_api_key>
+export EVENTSTREAMS_TOPIC_NAME=<event_streams_topic_name>
+export EVENTSTREAMS_BROKER_URL=<broker_urls_comma_separated>
+export EVENTSTREAMS_API_KEY=<event_streams_api_key>
